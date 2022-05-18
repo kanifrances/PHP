1785759d47b163d5d6cb89cfc8c40da932e12cb4
Kani frances <104614738+kanifrances@users.noreply.github.com>
2022-05-18 10:07:13 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,46 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<link rel="stylesheet" href="style.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Lists</title>
+</head>
+<body>
+
+<div class = "list">
+<h2>Ordered List items</h2>
+<ol>
+
+<?php
+
+$items  = "shoes, tie, shirt, shorts, blouse";
+$values =  explode(",",$items);
+foreach ($values as $str) {
+echo "<li> $str </li>";
+
+}
+
+?>
+</ol>
+
+<h2>Unordered List items</h2>
+<ul>
+
+<?php
+
+$items  = "shoes, tie, shirt, shorts, blouse";
+$values =  explode(",",$items);
+foreach ($values as $str) {
+echo "<li> $str </li>";
+
+}
+
+?>
+</ul>
+
+</div>
+</body>
+</html>
